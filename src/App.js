cab5b54d1912c184a538b49fd7675c4b6d42f5ce
Mohd Ebad <mohd88433@mastek.com>
2022-06-28 17:19:21 +0530

    Chef Page was Modified

--- src/App.js
+++ src/App.js
@@ -1,37 +1,36 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { Route } from "react-router-dom";
 
-import Home from './Pages/Home/Home'
-import About from './Pages/About/About'
-import Menu from './Pages/Menu/Menu'
-import Awards from './Pages/Awards/Awards'
-import Contact from './Pages/Contacts/Contact'
-import Navbar from './Components/Navbar/Navbar';
-import Footer from './Components/Footer/Footer'
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Menu from "./Pages/Menu/Menu";
+import Awards from "./Pages/Awards/Awards";
+import Contact from "./Pages/Contacts/Contact";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
 
 // import './App.css';
-
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
       <main>
-      <Route path="/home">
-        <Home />
-      </Route>
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/menu">
-        <Menu />
-      </Route>
-      <Route path="/awards">
-        <Awards />
-      </Route>
-      <Route path="/contact">
-       <Contact />
-      </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/menu">
+          <Menu />
+        </Route>
+        <Route path="/awards">
+          <Awards />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
       </main>
       <Footer />
     </React.Fragment>
