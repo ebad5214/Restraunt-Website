f1d219feeff75c68fdb25fdb71aeef8234dd5764
Mohd Ebad <mohd88433@mastek.com>
2022-07-06 12:52:41 +0530

    Some Code Cleaning was Done

--- src/Pages/Login/Register.js
+++ src/Pages/Login/Register.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Login from './main/Login'
 
-// import './Register.css'
+
 
 const Register = () => {
   return (
--- src/Pages/Menu/Menu.js
+++ src/Pages/Menu/Menu.js
@@ -1,14 +1,10 @@
 import React from 'react'
-// import Footer from '../Home/Footer/Footer'
-// import Navbar from '../Home/Navbar/Navbar'
 import SpecialMenu from './Main/SpecialMenu'
 
 const Menu = () => {
   return (
     <React.Fragment>
-        {/* <Navbar /> */}
         <SpecialMenu />
-        {/* <Footer /> */}
     </React.Fragment>
   )
 }
